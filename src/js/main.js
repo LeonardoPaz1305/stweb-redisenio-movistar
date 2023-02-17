0db8e7d4826c6e10586a989db91b1e979eb2651c
LeonardoPaz1305 <lavargas@stefanini.com>
2023-02-17 17:32:01 -0500

    footer culminado

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,41 @@
 // Imports
 //=require jquery/dist/jquery.min.js
-//=require bootstrap/js/dist/collapse.js
+
 //=require swiper/swiper-bundle.min.js
 
-document.addEventListener('DOMContentLoaded', function () {
+    //this is the button
+	var acc = document.getElementsByClassName("collapsible__header");
+	var i;
 
-});
+	for (i = 0; i < acc.length; i++) {
+		//when one of the buttons are clicked run this function
+	  acc[i].onclick = function() {
+		//variables
+		var panel = this.nextElementSibling;
+		var coursePanel = document.getElementsByClassName("collapsible__body");
+		var courseAccordion = document.getElementsByClassName("collapsible__header");
+		var courseAccordionActive = document.getElementsByClassName("collapsible__header active");
 
-var MyApp = {
-    //menu: function () { }
-}
-
-$(function () {
-    //MyApp.menu();
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-    
-});
+		/*if pannel is already open - minimize*/
+		if (panel.style.maxHeight){
+			//minifies current pannel if already open
+			panel.style.maxHeight = null;
+			//removes the 'active' class as toggle didnt work on browsers minus chrome
+			this.classList.remove("active");
+		} else { //pannel isnt open...
+			//goes through the buttons and removes the 'active' css (+ and -)
+			for (var ii = 0; ii < courseAccordionActive.length; ii++) {
+				courseAccordionActive[ii].classList.remove("active");
+			}
+			//Goes through and removes 'activ' from the css, also minifies any 'panels' that might be open
+			for (var iii = 0; iii < coursePanel.length; iii++) {
+			  this.classList.remove("active");
+			  coursePanel[iii].style.maxHeight = null;
+			}
+		  //opens the specified pannel
+		  panel.style.maxHeight = panel.scrollHeight + "px";
+		  //adds the 'active' addition to the css.
+		  this.classList.add("active");
+		} 
+	  }//closing to the acc onclick function
+	}//closing to the for loop.
